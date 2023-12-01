e108c9a847333d23d741ab968d4fbee2c2f9c331
Wooyul Chung <ttmac@Wooyuls-MacBook-Air.local>
2023-12-01 22:09:21 +0900

    3rd

--- src/computer/computer.module.ts
+++ src/computer/computer.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ComputerController } from './computer.controller';
+import { DiskModule } from 'src/disk/disk.module';
+import { CpuModule } from 'src/cpu/cpu.module';
 
 @Module({
-  controllers: [ComputerController]
+  imports: [CpuModule, DiskModule],
+  controllers: [ComputerController],
+
 })
 export class ComputerModule {}
